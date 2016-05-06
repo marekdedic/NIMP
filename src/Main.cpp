89cf4799e77c0d0a65db8c5875716fed2d3183ac
Marek Dědič <developer@dedic.eu>
2016-05-06 18:08:02 +0200

    Using WidgetRegistry and SwitchingWidget menus

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,8 +15,22 @@ int main(int argc, char** argv)
     WINDOW->setWindowTitle("NIMP");
     WINDOW->setWindowIcon(QIcon(":/icons/app_icon"));
 
-    SPLITTER->addWidget(new QLabel{"NIMP2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"});
-    SPLITTER->addWidget(new QLabel{"NIMP2bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"});
+    /*WidgetRegistry::getRegistry()->addItem();
+    WidgetRegistry::getRegistry()->addItem("Type1", []()->QWidget* {return new QLabel{"Type 1 Label"};}, [](SwitchingBar* bar, QWidget*)->void {
+        QMenu* menu{new QMenu{"Moje první menu"}};
+        bar->addMenu(menu);
+        QMenu* menu2{new QMenu{"Moje druhé menu"}};
+        menu2->addAction(new QAction{"Nw", 0});
+        menu2->addAction(new QAction{"Close", 0});
+        bar->addMenu(menu2);
+        QLabel* lab{new QLabel{"Moje třetí ne-menu"}};
+        bar->addWidget(lab);
+    });
+    WidgetRegistry::getRegistry()->addItem(new RegistryItem{"Type2", []()->QWidget* {return new QLabel{"Type 2 Label"};}});*/
+
+    /*SPLITTER->addWidget(new QLabel{"NIMP2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"});
+    SPLITTER->addWidget(new QLabel{"NIMP2bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"});*/
+    SPLITTER->addWidget();
 
     WINDOW->show();
 
